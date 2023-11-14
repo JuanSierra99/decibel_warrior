1d92db4afa0cce9fa7f1785405660dcee8929eb6
Juan Sierra <sierra.juan99@gmail.com>
2023-11-13 20:02:06 -0800

    Update to Sound Effects, and ui for game

--- cl411P1Sierra/App.js
+++ cl411P1Sierra/App.js
@@ -9,36 +9,50 @@ import {
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SettingsPage from './SettingsPage';
-import Svg, {Circle} from 'react-native-svg';
+import Svg, {Circle, Line} from 'react-native-svg';
 import Sound from 'react-native-sound';
 import AppContext from './AppContext';
 import AboutPage from './AboutPage';
 
 const Stack = createNativeStackNavigator();
-Sound.setCategory('Playback');
-const sound = new Sound('baby_cry.mp3', Sound.MAIN_BUNDLE, error => {
-  if (error) {
-    console.error('failed to load the sound', error);
-  }
-});
-sound.setNumberOfLoops(-1);
-
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+// This is a class component. Serves similar purpose as functional component
+// Im writing this as a class component just to learn how to write them. All my
+// Other components will be functional components.
 class FollowRadiusGame extends Component {
+  // constructor to set the values for class
   constructor(props) {
     super(props);
 
     this.state = {
-      radiusX: 150, // Initial radius X position
-      radiusY: 150, // Initial radius Y position
+      radiusX: 300, // Initial radius X position
+      radiusY: 300, // Initial radius Y position
       fingerX: 0, // Initial finger X position
       fingerY: 0, // Initial finger Y position
       inRadius: false,
       sound: props.soundEffect,
     };
   }
+
+  // componentDidMount() {
+  //   this.interval = setInterval(() => {
+  //     {
+  //       /*It's recommended to use the functional form of setState
+  //       when the new state depends on the previous state. */
+  //     }
+  //     this.setState(prevState => ({radiusX: prevState.radiusX - 3}));
+  //     if (this.state.radiusX == 0) {
+  //       this.setState({radiusX: 300});
+  //     }
+  //   }, 10);
+  // }
+
+  // componentWillUnmount() {
+  //   // Clear the interval when the component is unmounted
+  //   clearInterval(this.interval);
+  // }
 
   // Initialize PanResponder for tracking finger movements
   panResponder = PanResponder.create({
@@ -63,7 +77,7 @@ class FollowRadiusGame extends Component {
     );
 
     // Define a threshold for being within the radius
-    const radiusThreshold = 30;
+    const radiusThreshold = 45;
 
     // User is within the radius
     if (distance < radiusThreshold) {
@@ -98,14 +112,23 @@ class FollowRadiusGame extends Component {
       <View
         style={{
           flex: 1,
-          backgroundColor: 'red',
+          backgroundColor: inRadius ? 'green' : 'red',
         }}
         {...this.panResponder.panHandlers}>
-        <Svg height="300" width="300">
+        <Svg height={windowHeight} width={windowWidth}>
           <Text>{String(this.state.inRadius)}</Text>
+          <Line
+            x1="0"
+            y1="10"
+            x2={windowWidth}
+            y2="10"
+            stroke="blue"
+            strokeWidth="20"
+          />
           <Circle cx={radiusX} cy={radiusY} r="30" fill="blue" />
+          <Circle cx={fingerX} cy={fingerY} r="15" fill="blue" />
         </Svg>
-        <View // The styling is what creates our little green circle so we can see where our finger is at
+        {/* <View // The styling is what creates our little green circle so we can see where our finger is at
           style={{
             position: 'absolute',
             left: fingerX,
@@ -113,9 +136,9 @@ class FollowRadiusGame extends Component {
             width: 20,
             height: 20,
             borderRadius: 10,
-            backgroundColor: 'green',
+            backgroundColor: 'blue',
           }}
-        />
+        /> */}
       </View>
     );
   }
@@ -124,9 +147,8 @@ class FollowRadiusGame extends Component {
 const Game = () => {
   const context = useContext(AppContext);
   return (
-    <View>
-      <FollowRadiusGame soundEffect={context.soundEffect}></FollowRadiusGame>
-    </View>
+    // If i wrap in view i dont see background. WHYYYYY ?!??!?!?!?
+    <FollowRadiusGame soundEffect={context.soundEffect}></FollowRadiusGame>
   );
 };
 
@@ -157,17 +179,46 @@ const TitleScreen = () => {
 };
 
 const App = () => {
-  const [soundEffect, setSoundEffect] = useState(sound);
+  // The sound that will play when user finger leaves safe zone radius
+  const [soundEffect, setSoundEffect] = useState(null);
+  useEffect(() => {
+    Sound.setCategory('Playback');
+    const sound = new Sound('snore.mp3', Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.error('failed to load the sound', error);
+      } else {
+        sound.setNumberOfLoops(-1);
+        setSoundEffect(sound);
+        // sound.play(success => {
+        //   if (success) {
+        //     console.log('Sound is playing');
+        //   } else {
+        //     console.log('Sound failed to play');
+        //   }
+        // });
+      }
+    });
+    return () => {
+      if (sound) {
+        sound.release();
+      }
+    };
+  }, []);
 
   const changeSound = file_name => {
-    sound.release;
-    setSoundEffect(
-      new Sound(file_name, Sound.MAIN_BUNDLE, error => {
-        if (error) {
-          console.error('failed to load the sound', error);
-        }
-      }),
-    );
+    if (soundEffect) {
+      soundEffect.stop(); // Stop the current sound
+      soundEffect.release(); // Release resources
+    }
+    const newSound = new Sound(file_name, Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.error('failed to load the sound', error);
+      } else {
+        newSound.setNumberOfLoops(-1);
+        // newSound.play();
+        setSoundEffect(newSound);
+      }
+    });
   };
   // // Context shared between all pages
   const context = {soundEffect, changeSound};
@@ -178,7 +229,11 @@ const App = () => {
           <Stack.Screen
             name="TitleScreen"
             component={TitleScreen}></Stack.Screen>
-          <Stack.Screen name="GameScreen" component={Game} />
+          <Stack.Screen
+            name="GameScreen"
+            component={Game}
+            options={{headerShown: false}}
+          />
           <Stack.Screen name="AboutScreen" component={AboutPage} />
           <Stack.Screen
             name="SettingsPage"
