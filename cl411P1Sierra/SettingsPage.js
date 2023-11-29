25e563e0ce720f727469a2e05a2fe23dc47b2cd6
Juan Sierra <sierra.juan99@gmail.com>
2023-11-28 17:47:14 -0800

    added buttons for all the new sounds

--- cl411P1Sierra/SettingsPage.js
+++ cl411P1Sierra/SettingsPage.js
@@ -1,14 +1,19 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {View, Text, Switch, TouchableOpacity, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import AppContext from './AppContext';
-import App from './App';
 
 const SettingsPage = () => {
   const navigation = useNavigation();
   const context = useContext(AppContext);
-  // const context = useContext(AppContext);
-  // const soundEnabled = context.soundEnabled;
+
+  const soundData = [
+    {id: 'wet_fart.mp3', label: 'WET FART'},
+    {id: 'baby_cry.mp3', label: 'CRY BABY'},
+    {id: 'snore.mov', label: 'SNORE'},
+    {id: 'alarm.mp3', label: 'SIREN'},
+    {id: 'brother_gone.mp3', label: 'THAT BROTHER GONE'},
+  ];
 
   return (
     <View style={styles.container}>
@@ -21,21 +26,77 @@ const SettingsPage = () => {
           context.changeSound('wet_fart.mp3');
           console.log('Sound changed');
         }}>
-        <Text>Wet Fart</Text>
+        <Text>WET FART</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => {
           context.changeSound('baby_cry.mp3');
           console.log('Sound changed');
         }}>
-        <Text>Baby</Text>
+        <Text>CRY BABY</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => {
           context.changeSound('snore.mov');
           console.log('Sound changed');
         }}>
-        <Text>snore</Text>
+        <Text>SNORE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('siren.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>SIREN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('brother_gone.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>THAT BROTHER GONE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('bruh.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>BRUH</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('mimis.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>SLEEPY</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('my_hero.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>MY HERO</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('oh_my_god.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>OH MY GOD</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('pain_and_agony.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>PAIN AND AGONY</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          context.changeSound('thud.mp3');
+          console.log('Sound changed');
+        }}>
+        <Text>THUD</Text>
       </TouchableOpacity>
     </View>
   );
