50348f524e6e58fe9e40a2765f331b3d240c8af0
Juan Sierra <sierra.juan99@gmail.com>
2023-11-28 22:20:13 -0800

    SoundButton Component to clean up code. Added Styling

--- cl411P1Sierra/SettingsPage.js
+++ cl411P1Sierra/SettingsPage.js
@@ -1,121 +1,63 @@
-import React, {useContext, useState} from 'react';
-import {View, Text, Switch, TouchableOpacity, StyleSheet} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, {useContext} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 import AppContext from './AppContext';
+import style from './style';
+import {useNavigation} from '@react-navigation/native';
 
-const SettingsPage = () => {
-  const navigation = useNavigation();
+const SoundButton = props => {
+  const {name, file, color} = props;
   const context = useContext(AppContext);
-
-  const soundData = [
-    {id: 'wet_fart.mp3', label: 'WET FART'},
-    {id: 'baby_cry.mp3', label: 'CRY BABY'},
-    {id: 'snore.mov', label: 'SNORE'},
-    {id: 'alarm.mp3', label: 'SIREN'},
-    {id: 'brother_gone.mp3', label: 'THAT BROTHER GONE'},
-  ];
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>SELECT A SOUND</Text>
-      {/* <TouchableOpacity onPress={() => navigation.navigate('TitleScreen')}>
-        <Text>Return to Title Screen</Text>
-      </TouchableOpacity> */}
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('wet_fart.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>WET FART</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('baby_cry.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>CRY BABY</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('snore.mov');
-          console.log('Sound changed');
-        }}>
-        <Text>SNORE</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('siren.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>SIREN</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('brother_gone.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>THAT BROTHER GONE</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('bruh.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>BRUH</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('mimis.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>SLEEPY</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('my_hero.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>MY HERO</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('oh_my_god.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>OH MY GOD</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('pain_and_agony.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>PAIN AND AGONY</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          context.changeSound('thud.mp3');
-          console.log('Sound changed');
-        }}>
-        <Text>THUD</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        context.changeSound(file);
+        console.log('Sound changed');
+      }}>
+      <Text style={[style.Buttons, {color}]}>{name}</Text>
+    </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  soundToggle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-});
+const SettingsPage = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={style.body}>
+      <TouchableOpacity onPress={() => navigation.navigate('TitleScreen')}>
+        <Text style={style.returnButton}>Return to Title Screen</Text>
+      </TouchableOpacity>
+      <Text style={style.decibelWarriorText}>SELECT A SOUND</Text>
+      <SoundButton
+        name="WET FART"
+        file="wet_fart.mp3"
+        color="white"></SoundButton>
+      <SoundButton
+        name="CRY BABY"
+        file="baby_cry.mp3"
+        color="white"></SoundButton>
+      <SoundButton name="SNORE" file="snore.mov" color="white"></SoundButton>
+      <SoundButton name="SIREN" file="siren.mp3" color="white"></SoundButton>
+      <SoundButton
+        name="BROTHER GONE"
+        file="brother_gone.mp3"
+        color="white"></SoundButton>
+      <SoundButton name="BRUH" file="bruh.mp3" color="white"></SoundButton>
+      <SoundButton name="SNOOZY" file="mimis.mp3" color="white"></SoundButton>
+      <SoundButton name="HERO" file="my_hero.mp3" color="white"></SoundButton>
+      <SoundButton
+        name="OH MY GOD"
+        file="oh_my_god.mp3"
+        color="white"></SoundButton>
+      <SoundButton
+        name="PAIN AND AGONY !"
+        file="pain_and_agony.mp3"
+        color="white"></SoundButton>
+      <SoundButton
+        name="DORMHUB"
+        file="dormhub.mp3"
+        color="white"></SoundButton>
+      <SoundButton name="THUD" file="thud.mp3" color="white"></SoundButton>
+    </View>
+  );
+};
 
 export default SettingsPage;
